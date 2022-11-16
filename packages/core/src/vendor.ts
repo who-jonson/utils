856c50fe37c8e8180f7d56d52c399d69f1dac73a
Jonson B <www.jbc.bd@gmail.com>
2022-11-16 08:20:48 +0600

    feat: String Case transformers added ✨

--- packages/core/src/vendor.ts
+++ packages/core/src/vendor.ts
@@ -6,3 +6,10 @@ export {
   hasProperty as hasObjProp,
   deleteProperty as deleteObjProp
 } from 'dot-prop';
+
+export { camelCase } from 'camel-case';
+export { dotCase } from 'dot-case';
+export { pascalCase } from 'pascal-case';
+export { pathCase } from 'path-case';
+export { paramCase } from 'param-case';
+export { snakeCase } from 'snake-case';
