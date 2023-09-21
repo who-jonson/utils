723aae0ee29fea64f600fc44b5eb827edf6319f7
Jonson B <www.jbc.bd@gmail.com>
2023-09-22 05:05:58 +0600

    build: Updated dependencies to latest versions. 🎨

---
+++ scripts/core-types.ts
@@ -0,0 +1,20 @@
+import fs from 'fs-extra';
+import path from 'path';
+import { name } from './rollup.config';
+
+(async () => {
+  if (name === '@whoj/utils-core') {
+    const exp = [
+      'ConsistsOnlyOf',
+      'IsUpper',
+      'IsLower',
+      'CapitalizedWords',
+      'PascalCase',
+      'CamelCase',
+      'StringDigit'
+    ].map(e => `type ${e}`).join(', ');
+    let code = await fs.promises.readFile(path.resolve(process.cwd(), 'dist', 'index.d.ts'), 'utf8');
+    code = code.replace('export { type', `export { ${exp}, type`);
+    await fs.promises.writeFile(path.resolve(process.cwd(), 'dist', 'index.d.ts'), code, 'utf8');
+  }
+})();
